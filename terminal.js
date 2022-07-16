0fd7baa192ba504048ab67c68d2aad72217076b2
Snehil Pandey <76873947+Uthando993@users.noreply.github.com>
2022-07-16 20:07:40 +0530

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,6 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
 
 var con = 0;
 
